724d9d0cee98cb475be787705cc6b3c0f344175d
Carter Carlson <carlfarterson@gmail.com>
2019-09-21 14:02:07 -0500

    Create basic methods inside Bitmex class

--- py/bitmex.py
+++ py/bitmex.py
@@ -10,6 +10,7 @@ from py.config import *
 class Bitmex:
 
     def __init__(self, api_key, api_secret, test=True):
+        self.path = '/api/v1/'
         self.api_secret = api_secret
         self.headers = {'api-expires': '',
                         'api-key': api_key,
@@ -21,57 +22,65 @@ class Bitmex:
             self.url = 'https://bitmex.com'
 
 
+    def trade(self, symbol='XBTUSD'):
+
+        # Create message
+        query = urllib.parse.urlencode({'symbol': symbol})
+        path = self.path + 'trade?' + query
+        message = self.create_message(request='GET', path)
+        # Create signature
+        self.create_signature(request='GET', action='trade', symbol=symbol)
+        # Send request
+        self.send_request()
+
+
+    def create_signature(self, request, action, **query):
+        query = urllib.parse.urlencode(query)
+        path = self.path + action + '?' + query
+
+        self.update_headers(request, path)
+        self.send_request(path)
+
+
+    def update_headers(self, request, path):
+        api_expires = self.api_expires()
+        msg = request + path + api_expires
+        signature = hmac.new(bytes(self.api_secret, 'utf8'), bytes(message, 'utf8'), digestmod=hashlib.sha256).hexdigest()
+        self.headers['api-signature'] = signature
+
+
+    def send_request(self, path):
+        response = requests.get(self.url + path, headers=self.headers).json()
+        return response
+
+
     def api_expires(self, seconds=15):
         ''' UNIX timestamp after which the request is no longer valid.
         This prevents replay attacks.
         '''
         end_timestamp = (datetime.utcnow() + timedelta(seconds=seconds)).timestamp()
-        end_timestamp_str = str(int(end_timestamp))
-        return end_timestamp_str
-
-
-    def do_something():
-        # Create signature
-        create_signature()
-        # Send request
-        send_request()
-
-
-    def signature(self):
-        sig_hash
+        self.headers['api-expires'] = str(int(end_timestamp))
+        return self.headers['api-expires']
 
 
 
 
 
 
+# query = {'symbol': 'XBTUSD',
+#          'startTime': (datetime.utcnow() - timedelta(seconds=30)).isoformat(timespec='seconds')}
 
 
-
-
-
-query = {
-    'symbol': 'XBTUSD',
-    'startTime': (datetime.utcnow() - timedelta(seconds=30)).isoformat(timespec='seconds')
-}
-
-query = urllib.parse.urlencode(query)
-
-expires = str(int((datetime.utcnow() + timedelta(seconds=15)).timestamp()))
 
 path = '/api/v1/trade?'+ query
 msg = 'GET' + path + expires
 url = 'https://testnet.bitmex.com' + path
 
 
-signature = hmac.new(bytes(Secret_test, 'utf8'), bytes(msg, 'utf8'), digestmod=hashlib.sha256).hexdigest()
+
 
 api = {
     'api-expires': expires,
     'api-key': ID_test,
     'api-signature': signature
 }
-
-
-
-requests.get(url, headers=api).json()
